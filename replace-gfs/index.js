b77965b4fc582296777cde3ea65cec3d2e78eb25
mekwall <marcus.ekwall@gmail.com>
2019-01-24 22:16:03 +0100

    fix: replace graceful-fs with normalized-fs
    Necessary since we have devDependencies that rely on graceful-fs.

---
+++ replace-gfs/index.js
@@ -0,0 +1,15 @@
+if (process.env.NODE_ENV === 'test') {
+  const fs = require('fs');
+  module.exports = fs;
+  module.exports.gracefulify = () => fs;
+} else {
+  try {
+    const nfs = require('normalized-fs');
+    module.exports = nfs;
+    module.exports.gracefulify = nfs.normalize;
+  } catch (e) {
+    const nfs = require('../../');
+    module.exports = nfs;
+    module.exports.gracefulify = nfs.normalize;
+  }
+}
